468f35433801189554a472ffa0226e471108b28b
Ben Evans <bevans@zendesk.com>
2018-12-03 18:49:42 -0600

    first day both problems

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
 use std::io::{self, Read};
+use std::iter::Cycle;
+use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    println!("{:?}", buffer);
-    let mut result: i32 = 0;
-    for line in buffer.lines() {
-    	//let mut line: String = line.into();
-    	let (sign, rest) = line.split_at(1);
-    	let adjustment: i32 = rest.parse().unwrap();
-    	match sign {
-    		"-" => result -= adjustment,
-    		"+" => result += adjustment,
-    		_ => panic!("unexpected {:?}", sign)
-    	}
-    	println!("{:?}", sign);
-    	//
-    }
-    println!("{:?}", result);
+    println!("{:?}", problem_1_1(&buffer));
+    println!("{:?}", problem_1_2(&buffer));
     Ok(())
+}
+
+struct ChronalCalibration {
+	change_sequence: Cycle<std::vec::IntoIter<i32>>,
+	period: usize,
+	frequency: i32,
+}
+
+impl ChronalCalibration {
+    fn new(input: &str) -> ChronalCalibration {
+		let mut change_sequence = Vec::new();
+		for line in input.lines() {
+			let (sign, rest) = line.split_at(1);
+	    	let adjustment: i32 = rest.parse().unwrap();
+	    	match sign {
+	    		"-" => change_sequence.push((0 - adjustment)),
+	    		"+" => change_sequence.push(adjustment),
+	    		_ => panic!("unexpected {:?}", sign)
+	    	}
+		}
+		let period = change_sequence.len();
+		let cycle = change_sequence.into_iter().cycle();
+		ChronalCalibration{ change_sequence: cycle, period: period, frequency: 0}
+    }
+}
+
+impl Iterator for ChronalCalibration {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        self.frequency += self.change_sequence.next().unwrap();
+        Some(self.frequency)
+    }
+}
+
+fn problem_1_1(input: &str) -> i32 {
+	let mut frequencies = ChronalCalibration::new(input);
+	let period = frequencies.period;
+	frequencies.nth(period - 1).unwrap()
+}
+
+fn problem_1_2(input: &str) -> i32 {
+	let mut frequencies = ChronalCalibration::new(input);
+	let mut set: HashSet<_> = HashSet::new(); // dedup
+	
+	loop {
+		let item = frequencies.next().unwrap();
+		if !set.insert(item) {
+			return item;
+		}
+	}
 }
